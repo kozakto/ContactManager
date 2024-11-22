d7de34a0b0912a75182603be9b5ba8a1332d8fe1
Tomas Kozak <tomas.kozak@cross-ni.com>
2024-11-22 22:42:04 +0100

    added loger

--- src/main/java/com/example/ContactManager/controller/HomeController.java
+++ src/main/java/com/example/ContactManager/controller/HomeController.java
@@ -1,13 +1,20 @@
 package com.example.ContactManager.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
+    Logger logger = LoggerFactory.getLogger(HomeController.class);
+
     @GetMapping("/home")
     public String home() {
+
+        logger.info("Home method accessed");
+        logger.debug("Debugging Home method");
         return "Welcome to Contact Manager!";
     }
 }
