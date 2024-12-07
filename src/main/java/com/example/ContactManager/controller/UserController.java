c14401ec60fa81feab69840917f5ad13d82e4963
Tomas Kozak <tomas.kozak@cross-ni.com>
2024-12-07 14:22:55 +0100

    db connection update

--- src/main/java/com/example/ContactManager/controller/UserController.java
+++ src/main/java/com/example/ContactManager/controller/UserController.java
@@ -20,10 +20,10 @@ public class UserController {
     @GetMapping("/addUser")
     public String addUser() {
         Person person = new Person();
-        person.setFirstName("David");
-        person.setLastName("Moravec");
-        person.setAddress("Brno");
-        person.setPhoneNumber("159");
+        person.setFirstName("Jana");
+        person.setLastName("Nová");
+        person.setAddress("Plzen");
+        person.setPhoneNumber("135");
         userRepository.save(person);
         logger.info("User info: {}", person);
         return ("User added");
--- src/main/java/com/example/ContactManager/service/ContactService.java
+++ src/main/java/com/example/ContactManager/service/ContactService.java
@@ -5,29 +5,37 @@ import org.jooq.DSLContext;
 import org.jooq.Record;
 import org.jooq.Result;
 import org.jooq.impl.DSL;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+@Service
 public class ContactService {
-    private static final String URL = "jdbc:postgresql://localhost:5432/postgres";
-    private static final String USER = "postgres";
-    private static final String PASSWORD = "postgres";
+    @Value("${spring.datasource.url}")
+    private String URL;
+    @Value("${spring.datasource.username}")
+    private String USER;
+    @Value("${spring.datasource.password}")
+    private String PASSWORD;
+    Logger logger = LoggerFactory.getLogger(ContactService.class);
+
 
     public void fetchContacts() {
         try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD)) {
             DSLContext create = DSL.using(connection);
-            Result<Record> records = create.fetch("SELECT ID, FIRST_NAME, LAST_NAME, ADDRESS FROM PERSON");
+            Result<Record> records = create
+                    .select()
+                    .from(Person.PERSON).fetch();
 
             for (Record record : records) {
-                Long id = record.get(Person.PERSON.ID);
-                String firstName = record.get(Person.PERSON.FIRST_NAME);
-                String lastName = record.get(Person.PERSON.LAST_NAME);
-                String address = record.get(Person.PERSON.ADDRESS);
-
-                System.out.println("ID: " + id + ", First name: " + firstName + ", Last name: " + lastName + ", Address: " + address);
+                logger.info("Record: " + record);
             }
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
